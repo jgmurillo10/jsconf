4a224f80544d885f01843a593f062dfc74ceee4e
jgmurillo10 <jg.murillo10@uniandes.edu.co>
2023-11-15 13:16:27 -0500

    update main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,23 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
+    <html lang="en" className='dark'>
+      <body className={`${inter.className} dark:bg-gray-900 dark:text-white`}>
         {children}
+      <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+          <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 built with ❤️ from 🇨🇴☕️ by <a href="https://juanmurillo.co/" className="hover:underline">Juan Murillo</a>.
+          </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+              <li>
+                  <a href="https://juanmurillo.co" target="_blank" className="hover:underline me-4 md:me-6">About me</a>
+              </li>
+              <li>
+                  <a href="https://instagram.com/juanmurillo2" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
+              </li>
+          </ul>
+          </div>
+      </footer>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,69 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
+const demos = [
+  {
+    name: 'Basic',
+    description: `Basic example of integrating GPT into a React App, let's try to chat and maybe ask stuff about legal things, the weather, cool places`,
+    image: '',
+    path: '/basic'
+  },
+  {
+    name: 'Function',
+    description: 'This examples now integrates function calling which add steroids on GPT models, now were are able to use our own logic or third-party providers.',
+    image: '',
+    path: '/function'
+  },
+  {
+    name: 'Assistant',
+    description: 'Now we have super powers! We can define assistants in natural language, and integrate them through the API into our apps!',
+    image: '',
+    path: '/assistant'
+  },
+]
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hey JSConf!</h1>
+    <main className="">
+      <section>
+      <div
+        className="relative overflow-hidden rounded-lg bg-cover bg-no-repeat p-12 text-center"
+        style={{backgroundImage: `url('/medellin2.png')`, height: 900}}>
+        <div
+          className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
+          style={{backgroundColor: `rgba(0, 0, 0, 0.6)` }}>
+          <div className="flex h-full items-center justify-center">
+            <div className="text-white">
+              <h2 className="mb-4 text-4xl font-semibold">Hello <s>world</s> JSConf!</h2>
+              <h4 className="mb-6 text-xl font-semibold">Welcome to Supercharge your React apps with GPT demos!</h4>
+              <Link
+                href="/#demos"
+                className="rounded border-2 border-neutral-50 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                >
+                See demos
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      </section>
+      <section id="demos" className='flex flex-col items-center justify-between gap-4 p-8'>
+        {demos.map((demo) => (
+          <div className="max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+              <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+            </a>
+            <div className="p-5">
+              <Link href={demo.path} className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <h5>{demo.name}</h5>
+              </Link>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-200 mb-8">{demo.description}</p>
+              <Link href={demo.path} className="rounded border-2 border-neutral-50 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10">
+                Let's try it
+              </Link>
+            </div>
+          </div>
+        ))}
+      </section>
     </main>
   )
 }
