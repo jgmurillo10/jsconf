b9de3f01586ef5ee5d7c89e189cce331aee2e9e2
jgmurillo10 <jg.murillo10@uniandes.edu.co>
2023-11-16 14:24:15 -0500

    add max duration to speech func

--- src/app/api/speech/route.ts
+++ src/app/api/speech/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import OpenAI from "openai";
 
 export const runtime = 'edge';
+export const maxDuration = 300;
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
