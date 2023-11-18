9096f01c3068a00763c214d2466960e857c8665d
jgmurillo10 <jg.murillo10@uniandes.edu.co>
2023-11-17 23:24:34 -0500

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import { Analytics } from '@vercel/analytics/react';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -38,6 +40,7 @@ export default function RootLayout({
           </ul>
           </div>
       </footer>
+      <Analytics />
       </body>
     </html>
   )
